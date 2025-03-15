e2d5e186edf32f4f45e337243ef808724ea20494
Brajan Kukk <brkukk@taltech.ee>
2025-03-16 00:07:07 +0200

    basePath

--- self-triage-app/next.config.ts
+++ self-triage-app/next.config.ts
@@ -3,9 +3,11 @@ import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/selfTriageApp',
+  assetPrefix: '/selfTriageApp/', // Add this line
   images: {
     unoptimized: true,
   },
+  trailingSlash: true, // Add this line
 };
 
 export default nextConfig;
